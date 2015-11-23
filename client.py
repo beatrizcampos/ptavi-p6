f36e632ca7d434cddd0395182c7c5fe097ce8a1b
Your Name <beatrizcl.14@gmail.com>
2015-11-23 10:10:40 +0100

    Comienzo programa client.py - Leyendo argumentos

--- client.py
+++ client.py
@@ -5,20 +5,26 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
+try: 
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+    METHOD = sys.argv[1]
+    RECEIVER = sys.argv[2].split('@')[0]
+    IP = sys.argv[2].split('@')[1].split(':')[0]
+    PORT = sys.argv[2].split('@')[1].split(':')[1]
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+except IndexError:
+    print("Usage: python client.py method receiver@IP:SIPport")
+
+# Contenido que vamos a enviar (peticion SIP: INVITE sip:receptor@IP SIP/2.0)
+LINE = METHOD + ' sip:' + RECEIVER + '@' + IP + ' SIP/2.0\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP, PORT))
 
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
