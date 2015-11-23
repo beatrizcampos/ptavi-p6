5dd5ca84c261f861bca99d7da18d77c4804a338d
Your Name <beatrizcl.14@gmail.com>
2015-11-23 16:13:41 +0100

    Códigos de Respuesta - server.py

--- server.py
+++ server.py
@@ -13,8 +13,9 @@ try:
     PORT = sys.argv[2]
     FILE = sys.argv[3]
 
-except IndexError: 
+except IndexError:
     print("Usage: python server.py IP port audio_file")
+
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -25,18 +26,35 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         IP_CLIENT = str(self.client_address[0])
         print("LA IP DEL CLIENTE ES: " + IP_CLIENT)
-        self.wfile.write(b"Hemos recibido tu peticion")
+        #self.wfile.write(b"Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
+            method_client = line.decode('utf-8').split(' ')[0]
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
+            print("El cliente nos manda " + line.decode('utf-8'))
+            if method_client == 'INVITE':
+                rcv_answer = ("SIP/2.0 100 Trying" + '\r\n\r\n' +
+                             "SIP/2.0 180 Ringing" + '\r\n\r\n' +
+                             "SIP/2.0 200 OK" + '\r\n\r\n')
+                self.wfile.write(bytes(rcv_answer, 'utf-8'))
+
+            elif method_client == 'BYE':
+                answer = ("SIP/2.0 200 OK" + '\r\n\r\n')
+                self.wfile.write(bytes(answer, 'utf-8'))
+                print("Finish. . .")
+
+            else:
+                answer = ("SIP/2.0 400 Bad Request" + '\r\n\r\n')
+                self.wfile.write(bytes(answer, 'utf-8'))
+
+
+
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = socketserver.UDPServer(((IP, int(PORT))), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
+    print("Listening...")
     serv.serve_forever()
